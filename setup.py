1edf52453b6d58243bde325a4924de3e3475998b
Michael Rooney <launchpad@rowk.com>
2008-11-13 23:07:15 -0500

    pretty functional setup.py now! however the bin isn't working :[

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os, glob
+
+#Create an array with all the locale filenames
+I18NFILES = []
+for filepath in glob.glob("locales/*/LC_MESSAGES/*.mo"):
+    targetpath = os.path.dirname(os.path.join("share/", filepath))
+    I18NFILES.append((targetpath, [filepath]))
 
 setup(
     name = "wxBanker",
@@ -8,13 +15,21 @@ setup(
     author = "Michael Rooney",
     author_email = "michael@wxbanker.org",
     url = "http://wxbanker.org",
+    download_url='https://launchpad.net/wxbanker/+download',
     package_dir = {'wxbanker': ''},
     packages = ["wxbanker", "wxbanker.art"],
-    requires = ["wx>=2.8",], ## correct?
+    #requires = ["wx>=2.8",], ## correct?
+    license='GNU GPL',
+    platforms='linux',
+    scripts = ['bin/wxbanker'],
     data_files = [
-        ("locales", "locales/*"), ## probably not that easy?
-        (desktopfileLocation, "wxbanker.desktop"), ##
-        ("/usr/bin", "wxbanker"), ## usr/bin is right?
-    ]
+        ("share/applications", ["wxbanker.desktop"]),
+        ('share/icons/hicolor/16x16/apps', ['images/16/wxbanker.png']),
+        ('share/icons/hicolor/24x24/apps', ['images/24/wxbanker.png']),
+        ('share/icons/hicolor/32x32/apps', ['images/32/wxbanker.png']),
+        ('share/icons/hicolor/48x48/apps', ['images/48/wxbanker.png']),
+        ('share/icons/hicolor/256x256/apps', ['images/256/wxbanker.png']),
+        ('share/pixmaps', ['images/48/wxbanker.png']),
+    ] + I18NFILES
 
 )
