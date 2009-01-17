60a8eb79d6b52a520b0cb45ec6e527a6a8a2ed16
Michael Rooney <mrooney@ubuntu.com>
2009-01-17 15:10:51 -0800

    fix the plotting algorithm to return a small delta instead of zero when there is only one days worth of data also fix the tests to work with the previous of change of using today as the last day if it is after the last transaction day

--- plotalgo.py
+++ plotalgo.py
@@ -8,6 +8,8 @@ def get(transactions, numPoints):
     """
     # First, rename the function for convenience
     >>> T = makeTransaction
+    >>> today = datetime.date.today()
+    >>> one = datetime.timedelta(1)
     >>> result = get([], 10)
     >>> result[0]
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -17,23 +19,36 @@ def get(transactions, numPoints):
     True
     >>> get([T("2008/1/6", 1)], 3)[0]
     [1.0, 1.0, 1.0]
-    >>> get([T("2008/1/6", 1), T("2008/1/7", 2)], 4)[0]
+    >>> get([T(today-one, 1), T(today, 2)], 4)[0]
     [1.0, 1.0, 1.0, 3.0]
-    >>> get([T("2008/1/6", 1), T("2008/1/7", 2)], 1)
-    ([3.0], datetime.date(2008, 1, 6), 1.0)
-    >>> get([T("2008/1/6", 1), T("2008/1/7", 2)], 2)[0]
+    >>> result = get([T(today-one, 1), T(today, 2)], 1)
+    >>> result[0]
+    [3.0]
+    >>> result[1] == today - one
+    True
+    >>> result[2]
+    1.0
+    >>> get([T(today, 1), T(today+one, 2)], 2)[0]
     [1.0, 3.0]
-    >>> get([T("2008/1/6", 1), T("2008/1/7", 2)], 3)[0]
+    >>> get([T(today, 1), T(today+one, 2)], 3)[0]
     [1.0, 1.0, 3.0]
-    >>> get([T("2008/1/1", 1), T("2008/1/10", 2)], 10)[0]
+    >>> get([T(today-one*9, 1), T(today, 2)], 10)[0]
     [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 3.0]
-    >>> get([T("2008/1/1", 1), T("2008/1/3", 2), T("2008/1/10", 3)], 10)[0]
+    >>> get([T(today, 1), T(today+one*2, 2), T(today+one*9, 3)], 10)[0]
     [1.0, 1.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 3.0, 6.0]
+    >>> result = get([T(today, 1)], 2)
+    >>> result[0]
+    [1.0, 1.0]
+    >>> result[1] == today
+    True
+    >>> result[2] != 0
+    True
     """
+    # Don't ever return 0 as the dpp, you can't graph without SOME x delta.
+    smallDelta = 1.0/2**32
+    
     if transactions == []:
-        # In this case, the delta we return is a very small number instead of
-        # zero because for plotting you need SOME delta.
-        return [0] * 10, datetime.date.today(), 1.0/2**32
+        return [0] * 10, datetime.date.today(), smallDelta
     
     transactions = list(sorted(transactions))
     
@@ -55,7 +70,7 @@ def get(transactions, numPoints):
         
         points.append(points[-1])
         
-    return points[:-1], startDate, daysPerPoint
+    return points[:-1], startDate, daysPerPoint or smallDelta
     
     
 if __name__ == "__main__":
