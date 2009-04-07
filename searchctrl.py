64d512b51b65fdccfd5b1ebce4889ef9bdb98e0a
Mike Rooney <mrooney@ubuntu.com>
2009-04-07 10:33:05 -0700

    make the more/less search options properly translatable (LP: #357002)

--- searchctrl.py
+++ searchctrl.py
@@ -35,7 +35,7 @@ class SearchCtrl(wx.Panel):
         self.searchInBox = bankcontrols.CompactableComboBox(self, value=self.searchInChoices[0], choices=self.searchInChoices, style=wx.CB_READONLY)
 
         # The More/Less button.
-        self.moreButton = bankcontrols.MultiStateButton(self, baseLabel="%s "+_("Options"), labelDict={True: _("More"), False: _("Less")}, state=True)
+        self.moreButton = bankcontrols.MultiStateButton(self, labelDict={True: _("More options"), False: _("Less options")}, state=True)
 
         self.matchChoices = [_("Amount"), _("Description"), _("Date")]
         self.matchBox = bankcontrols.CompactableComboBox(self, value=self.matchChoices[1], choices=self.matchChoices, style=wx.CB_READONLY)
