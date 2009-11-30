2851fb70e3daf0e16c4e253b3a4a1854f7d0d5e3
Michael Rooney <mrooney@ubuntu.com>
2009-11-29 20:08:21 -0500

    remove unused stuff from templater and add the ability to compile mo files from po files

--- templater.py
+++ templater.py
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with wxBanker.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, commands, shutil, pprint, tarfile
+import os, subprocess
 
 """
 Some strings are displayed dynamically, and so we need to "hard code"
@@ -32,65 +32,25 @@ _("Transact") # Keep this here for now, might want it.
 def gentemplate(name="wxbanker.pot"):
     """Generate a .pot template with the given name."""
     translatableFiles = [f for f in os.listdir(".") if f.endswith(".py")]
-    command = "xgettext %s" % " ".join(translatableFiles) + " --output=po/%s"%name
-    return commands.getstatusoutput(command)
+    subprocess.check_call(["xgettext"] + translatableFiles + ["--output=po/%s"%name])
 
-def export2import(tarOrExtracted):
-    """Launchpad does not support importing exported po files without some massage."""
-    try:
-        isTar = tarfile.is_tarfile(tarOrExtracted)
-    except Exception:
-        isTar = False
-
-    if isTar:
-        tar = tarfile.open(tarOrExtracted)
-        tar.extractall("tmp")
-        exportDir = "tmp"
-    elif os.path.isdir(tarOrExtracted):
-        exportDir = tarOrExtracted
-    else:
-        raise Exception("Must pass location of either tar.gz file or extracted directory")
-
-    os.chdir(exportDir)
-
-    def nameto(po, d):
-        """Rename the .po file appropriately and move it to where it belongs."""
-        print "naming %s..." % po
-        locale = po[po.find("-")+1:-3]
-        newname = "%s.po" % locale
-        os.rename(po, newname)
-        shutil.move(newname, d)
-
-    for x in os.listdir("po"):
-        path = os.path.join("po", x)
-        if x.endswith(".po"):
-            nameto(path, "po/")
-        else:
-            os.remove(path)
-
-    for item in os.listdir("."):
-        if os.path.isdir(item):
-            if item != "po":
-                # Sometimes there are more .po files in a domain directory.
-                # These need to be named and moved to ../po/
-                os.chdir(item)
-                for po in (f for f in os.listdir(".") if f.endswith(".po")):
-                    nameto(po, "../po/")
-                os.chdir("../")
-                os.rmdir(item)
-        elif item.endswith(".po"):
-            # Name it properly and move it to po/
-            nameto(item, "po/")
-
-    #tarfile.open("massaged.tar.gz", "w|gz")
-
+def compiletranslations():
+    poFiles = [f for f in os.listdir("po/") if f.endswith(".po")]
+    for poFile in poFiles:
+        locale = poFile[:-3]
+        path = "locale/%s/LC_MESSAGES/wxbanker.mo" % locale
+        if not os.path.exists(path):
+            os.makedirs(os.path.split(path)[0])
+        try:
+            subprocess.check_call(("msgfmt", "-cv", "po/%s"%poFile, "-o", path), stdout=subprocess.PIPE)
+        except:
+            print "FAILED: ", poFile
+    subprocess.check_call(("bzr", "add", "locale/"))
+        
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) > 1 and sys.argv[1] =="-lp":
-        export2import(sys.argv[2])
+    if len(sys.argv) > 1 and sys.argv[1] =="--compile":
+        compiletranslations()
     else:
-        result = gentemplate()
-        if result == (0, ''):
-            print "SUCCESS!"
-        else:
-            print "FAILURE:\n", result
+        gentemplate()
+
